2ee1d6626350863688a651222135264f97c15745
Vast <Vastlee@gmail.com>
2020-10-01 15:16:35 -0700

    Updated to latest Dev. Added Scripts Backin
    Previous versions removed Scripts. Added it back in.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,60 @@
+﻿// brought to you by Jenni
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    [SerializeField] private AudioSource levelMusic;
+    [SerializeField] private AudioSource pauseMusic;
+    [SerializeField] private AudioSource gameOver;
+    // menu music? 
+    
+
+    #region Monobehavior
+
+    private void Start() {
+        Subscribe();
+    }
+
+    private void OnEnable() {
+        Subscribe();
+    }
+
+    private void OnDisable() {
+        Unsubscribe();
+    }
+    #endregion
+
+    #region Methods
+
+    private void PlayLevelMusic() {
+        levelMusic.Play();
+    }
+
+    private void PlayPauseMusic() {
+        pauseMusic.Play();
+    }
+
+    private void PlayGameOverMusic() {
+        gameOver.Play();
+    }
+
+    private void Subscribe() {
+        Unsubscribe();
+        EventController.Instance.OnResume += PlayLevelMusic;
+        EventController.Instance.OnPause += PlayPauseMusic;
+        EventController.Instance.OnGameOver += PlayGameOverMusic;
+    }
+
+    private void Unsubscribe() {
+        EventController.Instance.OnResume -= PlayLevelMusic;
+        EventController.Instance.OnPause -= PlayPauseMusic;
+        EventController.Instance.OnGameOver -= PlayGameOverMusic;
+    }
+    #endregion
+
+
+
+
+
+}
