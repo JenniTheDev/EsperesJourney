2a7d711ae369404c2297360d7ee2603533438ccf
Jenni <justlittleoldmejenn@gmail.com>
2020-10-29 16:00:51 -0700

    Moved Game Objects being used into test scene
    Test scene is now the main level 1

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -15,25 +15,25 @@ public class InputManager : MonoBehaviour {
     [SerializeField] private KeyCode resumeButton = KeyCode.P;
     [SerializeField] private KeyCode exitGame;
 
-    [SerializeField] private Character character;
-    private IMoveableChar playerCharacter;
+   // [SerializeField] private Character character;
+  //  private IMoveableChar playerCharacter;
     private float vinput = 0.0f;
     private float hinput = 0.0f;
     #region Monobehavior
 
     private void Start() {
-        playerCharacter = character.GetComponent<IMoveableChar>();
+      //  playerCharacter = character.GetComponent<IMoveableChar>();
     }
 
     private void Update() {
 
         // moves character any direction by getting x & y
-        vinput = Input.GetAxis("Vertical");
-        hinput = Input.GetAxis("Horizontal");
+      //  vinput = Input.GetAxis("Vertical");
+      //  hinput = Input.GetAxis("Horizontal");
 
-        if (vinput != 0.0f || hinput != 0.0f) {
-            playerCharacter.Move(new Vector2(hinput, vinput));
-        }
+      //  if (vinput != 0.0f || hinput != 0.0f) {
+     //       playerCharacter.Move(new Vector2(hinput, vinput));
+      //  }
 
     }
 
