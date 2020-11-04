45d10fa608a5eaecb3505a4a68146249747d0e23
Riz <ReederLoveland@gmail.com>
2020-11-04 11:39:23 -0800

    Added a basic BreakableObject Script
    Functions as a basic version of ObjectDestroy based off of the tag Player_Attack

---
+++ Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player_Attack") BreakIt();
+    }
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "Player_Attack") BreakIt();
+    }
+
+    public void BreakIt()
+    {
+        Destroy(this.gameObject);
+    }
+}
