1eaaeb4463e3ec28b9d74c62cfe961acee3f3440
Jenni <justlittleoldmejenn@gmail.com>
2020-11-07 22:40:21 -0800

    Puzzle 1 Coding Complete

---
+++ Assets/Scripts/TriggerPlateNoKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlateNoKey : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+  
+    private ITriggerable triggeredItem;
+
+    private void Start() {
+        triggeredItem = door.GetComponent<ITriggerable>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+       
+            triggeredItem.TriggerExecute();
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+
+            triggeredItem.TriggerRelease();
+        
+
+    }
+}
