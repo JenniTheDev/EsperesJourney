3fe48de4c5a4a232c312953348067a689d0e8068
Digx7 <webberboy01@yahoo.com>
2020-11-13 00:06:17 -0800

    Update code

---
+++ Assets/Scripts/LookAtRotator.cs
@@ -0,0 +1,57 @@
+// Digx7
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtRotator : MonoBehaviour
+{
+    [Header ("Main")]
+      [SerializeField] private GameObject objectToLookAt;
+      [SerializeField] private Vector2 directionOfObject;
+      [SerializeField] private Vector3 distanceOfObject;
+    [Header ("Auto Set Up")]
+      [SerializeField] private string tagOfGameObjectToLookFor = "Player";
+      [SerializeField] private bool lookForGameObjectOnAwake = true;
+
+    // --- Updates ---------------------------
+    public void Awake(){
+      if(lookForGameObjectOnAwake) findGameObjectWithTag();
+    }
+
+    public void FixedUpdate() {
+      LookAtObject();
+    }
+
+    // --- Main ------------------------------
+    public void LookAtObject() {
+
+      // get direction of object
+      distanceOfObject = gameObject.transform.position - objectToLookAt.transform.position;
+      distanceOfObject = Vector3.Normalize(distanceOfObject);
+      directionOfObject = distanceOfObject;
+
+      // rotate object
+      float angle = Mathf.Atan2(directionOfObject.y, directionOfObject.x) * Mathf.Rad2Deg;
+      transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    public bool isObjectToLookAtSet(){
+      if (objectToLookAt == null) return false;
+      else return true;
+    }
+
+    // --- Get/Find --------------------------
+    public GameObject getObjectToLookAt(){
+      return objectToLookAt;
+    }
+
+    public void findGameObjectWithTag (){
+      GameObject[] _object = GameObject.FindGameObjectsWithTag(tagOfGameObjectToLookFor);
+
+      int i = 1;
+      if (i == _object.Length)
+      {
+          objectToLookAt = _object[0];
+      }
+    }
+}
