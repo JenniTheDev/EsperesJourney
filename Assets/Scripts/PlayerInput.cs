88149d616a184d806a116a00836117d801b32642
Digx7 <webberboy01@yahoo.com>
2020-11-19 16:35:37 -0800

    Fixed some more bugs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -55,7 +55,7 @@ public class PlayerInput : MonoBehaviour
         Player.Character.Move.performed += ctx => playerScript.moveDirection = ctx.ReadValue<Vector2>(); // This permantly binds the given inputs to the script with no need for any update function
         Player.Character.Dash.performed += ctx => playerScript.Dash();
         Player.Character.Attack.performed += ctx => playerScript.Attack();
-        Player.Character.Blink.performed += ctx => playerScript.PlayerTeleport();
+        //Player.Character.Blink.performed += ctx => playerScript.PlayerTeleport();
         Player.Character.Shoot.performed += ctx => playerScript.ProjectileAbility();
         Player.Character.Heal.performed += ctx => playerScript.updateCurrentHealthPacks(-1);
     }
