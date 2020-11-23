5ff93dcc7424920f0d02592a44ae8f3e270d826b
Jenni <justlittleoldmejenn@gmail.com>
2020-11-22 19:05:27 -0800

    Trying Instance

--- Assets/Scripts/InstantSuccessTrigger.cs
+++ Assets/Scripts/InstantSuccessTrigger.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +10,7 @@ public class InstantSuccessTrigger : MonoBehaviour {
     // for only one key
     // [SerializeField] private Key.KeyType keyType;
     // For a list of keys
-    [SerializeField] private List<Key.KeyType> keyList;
+    [SerializeField] private List<Key.KeyType> correctKeyList;
     [SerializeField] private List<Key.KeyType> playerKeyList;
     private ITriggerable triggeredItem;
     [SerializeField] private int numOfCorrectKeys = 0;
@@ -19,63 +20,75 @@ public class InstantSuccessTrigger : MonoBehaviour {
 
     void Start() {
         triggeredItem = door.GetComponent<ITriggerable>();
-        numCorrectExpected = keyList.Count;
+        numCorrectExpected = correctKeyList.Count;
        
     }
 
     // Update is called once per frame
     void Update() {
+        //KeyHolder keyHolder = player.GetComponent<KeyHolder>();
+        //playerKeyList = keyHolder.GetKeyList();
+        //if (keyHolder != null && playerKeyList.Count <= numCorrectExpected) {
+        //    for (int i = 0; i < playerKeyList.Count; i++) {
+        //        if (playerKeyList[i] == keyList[i]) {
+        //            numOfCorrectKeys++;
+        //        }
+        //    }
+        //}
+        //// if correct
+        //if (numOfCorrectKeys == numCorrectExpected) {
+        //    keyHolder.ResetKeyList();
+        //    triggeredItem.TriggerExecute();
+        //    numOfCorrectKeys = 0;
+        //    gameObject.SetActive(false);
+        //    // if not correct
+        //} else if (playerKeyList.Count == numCorrectExpected && numOfCorrectKeys < numCorrectExpected ) {
+        //    doorFailSound.Play();
+        //    keyHolder.ResetKeyList();
+        //    numOfCorrectKeys = 0;
+        //}
+
         KeyHolder keyHolder = player.GetComponent<KeyHolder>();
         playerKeyList = keyHolder.GetKeyList();
-        if (keyHolder != null && playerKeyList.Count <= numCorrectExpected) {
-            for (int i = 0; i < playerKeyList.Count; i++) {
-                if (playerKeyList[i] == keyList[i]) {
-                    numOfCorrectKeys++;
-                }
-            }
-        }
-        // if correct
-        if (numOfCorrectKeys == numCorrectExpected) {
+        if (playerKeyList.SequenceEqual(correctKeyList)) {
             keyHolder.ResetKeyList();
             triggeredItem.TriggerExecute();
-            numOfCorrectKeys = 0;
             gameObject.SetActive(false);
-            // if not correct
-        } else if (playerKeyList.Count >= numCorrectExpected || (playerKeyList.Count == numCorrectExpected && numOfCorrectKeys < numCorrectExpected)) {
+        } else if (!playerKeyList.SequenceEqual(correctKeyList)) {
             doorFailSound.Play();
             keyHolder.ResetKeyList();
             numOfCorrectKeys = 0;
+
         }
 
 
 
+            // If Keys are being used and the key objects are being destroyed, respawn key objects here
+            // keyHolder.ResetKeyList();
+            //}
 
-        // If Keys are being used and the key objects are being destroyed, respawn key objects here
-        // keyHolder.ResetKeyList();
-        //}
+            // This crashes Unity
+            //    while (playerKeyList.Count < 5) {
+            //        for (int i = 0; i < playerKeyList.Count; i++) {
+            //            if (playerKeyList[i] == keyList[i]) {
+            //                numOfCorrectKeys++;
+            //            }
+            //        }
+            //        if (numOfCorrectKeys == numCorrectExpected) {
+            //            keyHolder.ResetKeyList();
+            //            triggeredItem.TriggerExecute();
+            //            numOfCorrectKeys = 0;
+            //            gameObject.SetActive(false);
+            //        } else if (playerKeyList.Count > numCorrectExpected) {
+            //            doorFailSound.Play();
+            //            keyHolder.ResetKeyList();
+            //            numOfCorrectKeys = 0;
+            //        }
 
-        // This crashes Unity
-        //    while (playerKeyList.Count < 5) {
-        //        for (int i = 0; i < playerKeyList.Count; i++) {
-        //            if (playerKeyList[i] == keyList[i]) {
-        //                numOfCorrectKeys++;
-        //            }
-        //        }
-        //        if (numOfCorrectKeys == numCorrectExpected) {
-        //            keyHolder.ResetKeyList();
-        //            triggeredItem.TriggerExecute();
-        //            numOfCorrectKeys = 0;
-        //            gameObject.SetActive(false);
-        //        } else if (playerKeyList.Count > numCorrectExpected) {
-        //            doorFailSound.Play();
-        //            keyHolder.ResetKeyList();
-        //            numOfCorrectKeys = 0;
-        //        }
-
-        //    }
-
+            //    }
 
 
+
+        }
     }
-}
 
