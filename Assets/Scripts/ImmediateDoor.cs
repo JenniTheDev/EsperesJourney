4e57014193ceba8392972c0f44f97c74806268e5
Jenni <justlittleoldmejenn@gmail.com>
2020-12-05 23:05:39 -0800

    Animating Door 1

--- Assets/Scripts/ImmediateDoor.cs
+++ Assets/Scripts/ImmediateDoor.cs
@@ -3,6 +3,9 @@ public class ImmediateDoor : Door {
 
     protected override void OpenDoor() {
         EventController.Instance.BroadcastDoorOpen();
+       // doorAnims.Play("");
+        // when animation is over
+        
         gameObject.SetActive(false);
     }
 }
