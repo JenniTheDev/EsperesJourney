c023487ac22058cf1d5ec6060a7c5097546ec0fc
Luis <xxcmgxx02@gmail.com>
2021-01-14 17:14:40 -0800

    Dialogue 1.0
    Working dialogue with option to display choices.
    triggers using collider. Only when active bool is true. WIP

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,36 @@
+﻿//Luis
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+    public string name = "";
+    [Tooltip("Will choices (Answers/Responses) be displayed after the dialogue?")]
+    public bool isQuestion;
+
+    [Space]
+    [Header("Dialogue")]
+        [TextArea(3, 10)]
+        public string[] sentences;
+
+    
+
+    [Space]
+    //[HideInInspector]
+    [Header("Answers/Responses")]
+        [Tooltip("Answers list.")]
+        [TextArea(3, 10)]
+        public string[] answers;
+    //[HideInInspector]
+        [TextArea(3, 10)]
+        public string correctAnswer;
+    //[HideInInspector]
+        [TextArea(3, 10)]
+        public string correctResponse;
+    //[HideInInspector]
+        [TextArea(3, 10)]
+        public string incorrectResponse;
+
+
+}
