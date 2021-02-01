9092c4568711b80e5bd115f417d6e71e6b187d09
Digx7 <webberboy01@yahoo.com>
2021-02-01 14:32:31 -0800

    Working on new enemy prototypes

---
+++ Assets/Scripts/General/ObjectScaler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObjectScaler : MonoBehaviour
+{
+    /*public Vector3Event objectScale;
+
+    [SerilizeField] private GameObject objectToScale;
+    public enum ScaleMode {continus, finite};
+    [SerilizeField] private ScaleMode _scaleMode = ScaleMode.continus;*/
+}
