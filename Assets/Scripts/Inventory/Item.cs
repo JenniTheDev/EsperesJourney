3f761a758d380ddd95324e418a0b5638822e3b57
Luis <xxcmgxx02@gmail.com>
2021-03-09 23:34:44 -0800

    Inventory 1.1
    Added in Inventory Scripts folder.
    - Inventory, item, Item Assets, and UI Inventory scripts.
    Added in Inventory Canvas with UI Inventory game object.
    - Toggle input set to i<Keyboard>.
    Reworked Character Portrait game object.
    - Updated Dialogue Box and Answer Buttons sprites with concept art.

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum ItemType
+    {
+        Key,
+        Coin,
+        Document,
+    }
+
+    public ItemType itemType;
+    public int amount;
+
+    public Sprite GetSprite()
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.Coin: return ItemAssets.Instance.CoinSprite;
+            case ItemType.Key: return ItemAssets.Instance.KeySprite;
+            case ItemType.Document: return ItemAssets.Instance.DocumentSprite;
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/UI_Inventory.cs
@@ -0,0 +1,62 @@
+﻿//Luis
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inventory : MonoBehaviour
+{
+    private Inventory inventory;
+
+    [SerializeField] private Transform itemSlotContainer;
+    [SerializeField] private Transform itemSlotTemplate;
+    private int X_MAX = 9;
+    private int Y_MAX = 2;
+
+    private void Awake()
+    {
+        //itemSlotContainer = transform.Find("itemSlotContainer");
+        //if (itemSlotContainer == null) { Debug.LogError("itemSlotContainer was not found."); }
+        //itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
+        //if (itemSlotTemplate == null) { Debug.LogError("itemSlotTemplate was not found."); }
+        //Debug.Log("hello " + itemSlotTemplate);
+        this.gameObject.SetActive(false);
+    }
+    public void SetInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+        
+        RefreshInventoryItems();
+    }
+
+    private void RefreshInventoryItems()
+    {
+        int x = 0;
+        int y = 0;
+        float itemSlotCellSize = 95f;
+
+        foreach (Item item in inventory.GetItemList())
+        {
+            Debug.Log(itemSlotContainer); /////////////////////////////////////!!!!!!!!!!
+            Debug.Log(itemSlotTemplate);
+
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            itemSlotRectTransform.gameObject.SetActive(true);
+            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
+            Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
+            image.sprite = item.GetSprite();
+            x++;
+            if(x > X_MAX - 1)
+            {
+                x = 0;
+                y--;
+            }
+        }
+    }
+
+    public void ToggleInventory()
+    {
+        if (this.gameObject.activeSelf) { this.gameObject.SetActive(false); }
+        else { this.gameObject.SetActive(true); }
+    }
+}
